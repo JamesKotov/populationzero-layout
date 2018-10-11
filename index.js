5b6d323de424ec637258142593497340bab24357
Eugene Strigo <e.strigo@onlinepay.com>
2018-10-12 00:32:53 +0300

    Modal dialog example

--- index.js
+++ index.js
@@ -2,7 +2,8 @@
 
 import 'bootstrap/js/dist/util';
 import 'bootstrap/js/dist/dropdown';
-import 'bootstrap/js/dist/collapse';
+import 'bootstrap/js/dist/modal';
+// import 'bootstrap/js/dist/collapse';
 
 import './assets/css/styles.scss';
 
